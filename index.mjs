1f4f90ef1ae90717517aaa1655eea2c15cd57f35
Paul Min <info@paulmin.nl>
2026-04-27 13:31:08 +0200

    chore: update dependencies

--- index.mjs
+++ index.mjs
@@ -1,12 +1,12 @@
-import { setOutput, setFailed } from '@actions/core';
-import repoDetails from './input.mjs';
+import { setOutput, setFailed } from "@actions/core";
+import repoDetails from "./input.mjs";
+import { Octokit } from "@octokit/core";
 
 try {
   let release = await new Octokit().repos.getLatestRelease(repoDetails());
-  setOutput('latest', release.data.tag_name);
+  setOutput("latest", release.data.tag_name);
 } catch (error) {
   if (error instanceof Error) {
     setFailed(error);
   }
 }
-
